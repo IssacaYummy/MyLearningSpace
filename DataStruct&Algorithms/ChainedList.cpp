a280f7f46e4edd8a7f2b9e4012fbb7d60d614be8
Yummy's_Ubuntu <3047863478@qq.com>
2025-09-30 00:21:51 +0800

    Studying ChainedList

---
+++ DataStruct&Algorithms/ChainedList.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <string>
+
+#define MAXSIZE 100
+
+#define OK 1
+#define ERROR 0
+
+typedef int Status;
+typedef std::string ElemTpye;
+
+typedef struct Node
+{
+    ElemTpye data;
+    Node* Next;
+}Node;
+
+typedef Node* LinkList;
+
+/* Function Declaration */
+Status GetElem(LinkList L, int pos, ElemTpye &e);
+Status InsertElem(LinkList L, int pos, ElemTpye e);
+Status DeliteElem(LinkList L, int pos);
+
+int main()
+{
+
+}
+
+
+/**
+ * @brief Get one sepecific Element
+ */
+Status GetElem(LinkList L, int pos, ElemTpye &e)
+{   
+    if( pos < 1)
+    {
+        std::cout << "输入的值不合法" << std::endl;
+        return ERROR;
+    }
+
+    int i = 1;
+    LinkList p = L->Next;
+
+    while(p && i < pos)
+    {
+        p = p->Next;
+        ++i;
+    }
+
+    if(!p || i > pos)
+    {
+        return ERROR;
+    }
+
+    e = p->data;
+
+    std::cout << "GetSuccessfully" << "\n";
+    std::cout << "第" << pos << "个值为" << e << std::endl;
+
+    return OK;
+}
+
+
+Status InsertElem(LinkList L, int pos, ElemTpye e)
+{
+    if(pos < 1) return ERROR;
+
+    int i = 1;
+    LinkList p = L->Next;
+
+    while(p && i < pos-1)
+    {
+        p = p->Next;
+        i++;
+    }
+
+    if(!p)
+    {
+        std::cout << "插入位数超过链表最大数值" << std::endl;
+        return ERROR;
+    }
+
+    LinkList s = (LinkList)new(Node);
+    s->Next = p->Next;
+    p->Next = s;
+    s->data = e;
+
+    std::cout << "in pos" << pos << "insert \"" << e <<"\" successful" << std::endl;
+
+    return OK;
+}
+
+
+
+Status DeleteElem(LinkList L, int pos, ElemTpye &e)
+{
+    if(pos < 1) return ERROR;
+
+    int i = 1;
+    LinkList p = L->Next;
+
+    while(p && i < pos-1)
+    {
+        p = p->Next;
+        i++;
+    }
+
+    if(!p || !(p->Next))
+    {
+        std::cout << "pos超过链表长度或要删除的结点不存在" << std::endl;
+        return ERROR;
+    }
+
+    LinkList q = p->Next;    
+    p->Next = q->Next;
+    e = p->data;
+
+    delete q;
+    
+    return OK;
+}
